d356f40fa1eda98834f9a7d3182b3654ddeef7b0
Bilal Khoukhi <hellO@bilalkhoukhi.com>
2019-01-23 08:09:41 -0600

    Changes name of imported component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 import H2 from "./H2";
-import Navbar from "./Navbar";
+import HeaderWrapper from "./HeaderWrapper";
 import Grow from "../Grow";
 import LogoIcon from "../LogoIcon";
 import GithubIcon from "../GithubIcon";
 
 const Header = props => {
   return (
-    <Navbar {...props}>
+    <HeaderWrapper {...props}>
       <LogoIcon />
       <H2>Web Screen Shooter</H2>
       <Grow />
       <GithubIcon href="https://github.com/Bilal-io/Web_Screen_Shooter-React-Selenium" />
-    </Navbar>
+    </HeaderWrapper>
   );
 };
 
